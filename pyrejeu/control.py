2d7718af7cbc2fe7fb7f91f336df2d849e47b933
Audrey <audrey.gabaud@gmail.com>
2017-01-22 11:14:08 +0100

    fct set_heading

--- pyrejeu/control.py
+++ pyrejeu/control.py
@@ -8,6 +8,9 @@ from math import *
 Session = sessionmaker(bind=mod.engine)
 session = Session()
 
+
+
+# Calcul des nouveaux plots à partir de la cmde de changement de cap.
 def set_heading(f_id, heading, time): # time en sec
 
     incrementation_time = 8 # On crée des plots successifs espacés dans le temps de 8sec (comme sur exemple_donnees.txt).
@@ -15,10 +18,13 @@ def set_heading(f_id, heading, time): # time en sec
     # On prend dans la bdd tout les plots à partir desquels la cmde de changement de cap a été envoyée
     # + le plot juste avant l'ordre de cmde (si la cmde n'a pas été envoyée sur une heure multiple de 8).
     list_cones_sup_time = session.query(mod.Cone).filter \
-        (mod.Cone.flight_id == f_id, mod.Cone.hour >= (time-incrementation_time+1), mod.Cone.version == mod.Cone.last_version)
+        (mod.Cone.flight_id == f_id, mod.Cone.hour >= (time-incrementation_time+1), mod.Cone.version == mod.Cone.flight.last_version)
     starting_cone = list_cones_sup_time[0] # Plot de départ.
     last_cone = list_cones_sup_time[-1] # Dernier plot de la dernière trajectoire de l'avion.
     list_cones = [starting_cone] # liste_cones contient strating_cone en 1er élément et sera complété des plots calculés à partir du nouveau cap.
+
+    # On incrémente last_version du vol associé à l'avion.
+    mod.Cone.flight.last_version += 1
 
     # On considère que la vitesse de l'avion sera constante.
     speed_vector_norm = math.sqrt(starting_cone.vit_x**2 + starting_cone.vit_y**2)  # Calcul de la norme du vecteur vitesse au plot de départ.
@@ -33,7 +39,7 @@ def set_heading(f_id, heading, time): # time en sec
 
         # Définition du nouveau plot.
         # On considère que l'avion restera à la même altitude.
-        n_cone = mod.Cone(version=starting_cone.last_version+1, last_version=starting_cone.last_version+1,
+        n_cone = mod.Cone(version=starting_cone.last_version+1,
                  pos_x=n_pos_x, pos_y=n_pos_y,
                  vit_x=n_vit_x, vit_y=n_vit_y,
                  flight_level=starting_cone.flight_level, rate=0, tendency=0,
@@ -42,3 +48,12 @@ def set_heading(f_id, heading, time): # time en sec
         session.add(n_cone) # On ajoute n_cone à la bdd.
         list_cones.append(n_cone)
 
+
+
+def delete_version(truc, machin):
+
+    mod.Cone.flight.last_version -= 1
+
+
+
+    pass
