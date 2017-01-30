57dfb585888523a1cc7456b4615bc3de9dc48013
APALMA <palma.a93@gmail.com>
2017-01-30 16:31:43 +0100

    rejeu_test commentaires

--- rejeu_tests.py
+++ rejeu_tests.py
@@ -7,7 +7,9 @@ from pyrejeutest import test_utils
 if __name__ == "__main__":
     x = 0
     while x!=4 :
+        #Demander à l'utilisateur ce qu'il souhaite faire
         print("\n Welcome in rejeu_test. What do you want to do ? \n [1] test importations.py \n [2] test utils.py \n [3] test everything \n [4] exit rejeu_test \n")
+        #Prendre en compte le choix de l'utilisateur
         x=input()
 
         if x==1 :
