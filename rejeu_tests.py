8f5981fbe8aff341206b49664937e2cd84b1ac56
APALMA <palma.a93@gmail.com>
2017-01-30 15:44:51 +0100

    rejeu tests final

---
+++ rejeu_tests.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+from pyrejeutest import test_importations
+from pyrejeutest import test_utils
+
+if __name__ == "__main__":
+    x = 0
+    while x!=4 :
+        print("\n Welcome in rejeu_test. What do you want to do ? \n [1] test importations.py \n [2] test utils.py \n [3] test everything \n [4] exit rejeu_test \n")
+        x=input()
+
+        if x==1 :
+            os.system("python ./pyrejeutest/test_importations.py")
+        elif x==2 :
+            os.system("python ./pyrejeutest/test_utils.py")
+        elif x==3 :
+            os.system("python ./pyrejeutest/test_importations.py")
+            os.system("python ./pyrejeutest/test_utils.py")
+        elif x==4 :
+            print("Exiting test... Good bye !")
+        else :
+            print("\n Please choose a number between 1 and for")
+
+
+
+
+
